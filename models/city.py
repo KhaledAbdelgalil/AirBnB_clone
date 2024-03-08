6a4fb6cc72343e0dd2451d2c27272a399239378f
mariamabdelgalil <mariam.ahmad.200300@gmail.com>
2024-03-07 16:45:40 -0800

    city class

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""City Module."""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    City class for managing city information.
+
+    Attributes:
+        state_id (str): The ID of the associated state.
+        name (str): The name of the city.
+    """
+    state_id = ""
+    name = ""
