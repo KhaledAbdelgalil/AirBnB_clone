b0b20303e6b4589ed13820ed50bda004308a3ae7
mariamabdelgalil <mariam.ahmad.200300@gmail.com>
2024-03-07 16:55:58 -0800

    review class

---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Review module."""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """A class representing a review """
+    place_id = ""
+    user_id = ""
+    text = ""
